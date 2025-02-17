279881d699f4aaaa16eb117abd8b61d5296ec8a9
Kaleb Binger <kalebbinger@gmail.com>
2025-02-17 11:39:47 -0700

    more experimenting

--- Integration/Int1.py
+++ Integration/Int1.py
@@ -12,19 +12,19 @@ from dependencies.chs.lib.protocol_resolver.roles.wit_protocol_resolver import W
 import dependencies.util_func as util_func
 
 # Declaring GPS Port and Baud
-GPSserial_port = "/dev/ttyUSB0"
-GPSbaud_rate = 9600
+GPSserial_port = "/dev/ttyUSB0" # GPS serial port
+GPSbaud_rate = 9600 		# Default GPS baud rate
 
 # Declaring IMU Port and baud
-IMUserial_port = "/dev/ttyAMA0"
-IMUbaud_rate = 9600
+IMUserial_port = "/dev/ttyAMA0" #IMU Serial port
+IMUbaud_rate = 9600		#Default IMU baud rate
 
 # Make GPS Serial object
-GPSser = serial.Serial(GPSserial_port, GPSbaud_rate, timeout=.3)
-GPSser.baudrate = GPSbaud_rate
-gps = adafruit_gps.GPS(GPSser, debug = False)
-gps.send_command(b"PMTK314,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-gps.send_command(b'PMTK220,200')
+GPSser = serial.Serial(GPSserial_port, GPSbaud_rate, timeout=.3) 		# Using our fancy serial import, create an object for the GPS with the port name, baud rate, and timeout
+GPSser.baudrate = GPSbaud_rate					 		# Change baudrate to wakeup GPS
+gps = adafruit_gps.GPS(GPSser, debug = False)			 		# Create gps object with adafruit parser
+gps.send_command(b"PMTK314,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")	# See adafruit documentation, telling GPS what data we want
+gps.send_command(b'PMTK220,200')						# See adafruit documentation, telling GPS how fast we want to update date
 
 # Make IMU Serial Object
 device = deviceModel.DeviceModel("IMU",WitProtocolResolver(),JY901SDataProcessor(),"51_0")
