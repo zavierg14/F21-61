776e31276971d5f687b347098dfd5fd86afca14f
Kaleb Binger <kalebbinger@gmail.com>
2025-02-18 23:51:19 -0700

    Testing v2 of integration code with .csv and lists

---
+++ Integration/Int2.py
@@ -0,0 +1,105 @@
+# Kaleb Binger 2/2025
+# F21-61 - Swap Hudson
+# This is v2 of the file moving from printing to txt to puting each value in a numpy array.
+# :)
+
+import csv
+import serial			# Serial to mess w sensors
+import time			# Lets us check what time it is
+import adafruit_gps		# Tells what the GPS is thinking
+import numpy as np		# We need to use some arrays
+import datetime			# Bro idek but the dependencies need it
+import sys			# ^^^^^^
+import platform			# ^^^^^^
+import struct			# ^^^^^^
+import dependencies.chs.lib.device_model as deviceModel	# Actual local file w the deviceModel class
+import dependencies.chs.JY901S as JY			# WitMotion's actual file that I'm stealing functions from :)
+from dependencies.chs.lib.data_processor.roles.jy901s_dataProcessor import JY901SDataProcessor	# WitMotion file to help w data parsing i think
+from dependencies.chs.lib.protocol_resolver.roles.wit_protocol_resolver import WitProtocolResolver	# ^^^^^
+import dependencies.util_funcTest as util_func	# My file :^) this is super useful
+
+# Sampling Frequency Stuff
+sampling_freq = 5 #Hz ---------- frequency we want
+interval = 1/sampling_freq #s -- time in seconds between samples
+
+# Declaring GPS Port and Baud
+GPSserial_port = "/dev/ttyUSB0" # GPS serial port
+GPSbaud_rate = 9600 		# Default GPS baud rate
+GPSdata = [[time.monotonic(),0,0,0,0,0]]	# Time/lat/long/altitude/speed/sat count
+
+# Declaring IMU Port and baud
+IMUserial_port = "/dev/ttyAMA0" # IMU Serial port
+IMUbaud_rate = 9600		# Default IMU baud rate
+IMUdata = [[time.monotonic(),0,0,0,0,0,0]] # Time/accX/accY/accZ/angleX/AngleY/AngleZ
+
+# Make GPS Serial object
+GPSser = serial.Serial(GPSserial_port, GPSbaud_rate, timeout=.3) 		# Using our fancy serial import, create an object for the GPS with the port name, baud rate, and timeout
+GPSser.baudrate = GPSbaud_rate					 		# Change baudrate to wakeup GPS
+gps = adafruit_gps.GPS(GPSser, debug = False)			 		# Create gps object with adafruit parser
+gps.send_command(b"PMTK314,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")	# See adafruit documentation, telling GPS what data we want
+gps.send_command(b'PMTK220,200')						# See adafruit documentation, telling GPS how fast we want to update date
+
+# Make IMU Serial Object
+device = deviceModel.DeviceModel("IMU",WitProtocolResolver(),JY901SDataProcessor(),"51_0")	# WitMotion libraries, make device object
+device.serialConfig.portName = IMUserial_port   						# Set serial port
+device.serialConfig.baud = IMUbaud_rate                 					# Set baud rate
+device.openDevice()                                 						# Open serial port
+
+# Housekeeping before recording data
+last_print = time.monotonic()	# Start time for sampling
+
+# Meat of recording and printing data
+try:							# Try & except to give a way of ending loop someday 
+	while True:					# While loop to continue checking sensors
+		gps.update()				# Command to make the parsing library check if theres anything new from GPS
+		current = time.monotonic()		# Check current time
+		if current - last_print >= interval:	# Compare elapsed time to sampling interval
+
+			# If the elapsed time is past the interval it will check the sensors 
+			last_print = current		# Set time since last sample to current time
+			if not gps.has_fix:		# Check for GPS fix
+
+				# Try again if we don't have a fix yet.
+				print("Waiting for fix...")
+				continue			# Continue loop until fix is obtained
+			print("=" * 40) 			# Print a separator line.
+			util_func.deviceWrite(gps, device, False)
+			imutemp = [time.monotonic(), device.getDeviceData("accX"), device.getDeviceData("accY"), device.getDeviceData("accZ"), device.getDeviceData("angleX"), device.getDeviceData("angleY"), device.getDeviceData("angleZ")]
+			gpstemp = [time.monotonic(), gps.latitude, gps.longitude, gps.altitude_m, gps.speed_kmh, gps.satellites]
+			GPSdata.append(gpstemp)
+			IMUdata.append(imutemp)
+
+except KeyboardInterrupt:	# Ctrl+C sends keyboard interupt and stops loop
+	pass			# Does literally nothing but stop python from whining
+
+# Close serial devices
+GPSser.close()			# Closes gps serial
+device.closeDevice()		# Closes IMU serial and stops thread - can take a few seconds don't freak out :)
+
+# Write to file
+print(GPSdata)
+util_func.csvWrite(GPSdata, "GPS.txt", ["Time", "Lat", "Long", "Alt", "Speed", "Sats"])
+
+
+
+##
+#~Camel~
+#   .--' |
+#  /___^ |     .--.
+#      ) |    /    \
+#     /  |  /`      '.
+#    |   '-'    /     \
+#    \         |      |\
+#     \    /   \      /\|
+#      \  /'----`\   /
+#      |||       \\ |
+#      ((|        ((|
+#      |||        |||
+#     //_(       //_(
+
+
+
+# Happy Driving
+# F21-61 - Kaleb Binger
+# soli deo gloria
+# Ιησους Χριστος
---
+++ Integration/dependencies/util_funcTest.py
@@ -0,0 +1,107 @@
+import time		# idek if i use this lol
+import datetime		# Used to name file
+import adafruit_gps	# ~Parses the GPS~
+import csv
+
+_IsWriteF = False	# Global variable to tell deviceWrite whether there is an open file
+_writeF = None		# Global variable to tell deviceWrite what file to write to
+
+# A fancy shamncy function to open a text file and give data headers to work with
+def startRecord():
+	global _writeF			# imports the global variables?
+	global _IsWriteF		#  ^^^^^^^
+	_writeF = open(str(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))+".txt","w")	# Opens file and names it the current year,month,day,hour,and second - assigns to _writeF
+	_IsWriteF = True		# Tells the other functions we have an open file
+	Tempstr = "Time"		# Begins the string to print into file
+	Tempstr += "\t\t\tLatitude\tLongitude\tAltitude"	# Continues string
+	Tempstr += "\tSpeed (km/hr)"				# Continues string
+	Tempstr += "\taccX (m/s^s)"				# Continues string
+	Tempstr += "\taccY (m/s^s)"				# Continues string
+	Tempstr += "\taccZ (m/s^s)"				# Continues string
+	Tempstr += "\tangleX (m/s^s)"				# Continues string
+	Tempstr += "\tangleY (m/s^s)"				# Continues string
+	Tempstr += "\tangleZ (m/s^s)"				# ^^^^^^
+	Tempstr += "\tSatellites\n"				# We get it at this point
+	_writeF.write(Tempstr)					# Writes that string to the file yippee!
+
+# Function to close file when were all done
+def endRecord():
+	global _writeF		# Imports variables
+	global _IsWriteF	# Imports variables
+	_IsWriteF = False	# NOT writing anymore
+	_writeF.close()		# File is closed!
+
+# This is the function of all time- it actually pulls the data from sensors and prints it to terminal and file
+def deviceWrite(gps, device, prt):	# Pass in gps and device objects and True/False on whether to print to console
+	if prt:				# IF print is true
+
+		# Huge long print statement... basically gps."insert your thing here" calls a gps value
+		# and device.getDeviceData("insert your thing here") calls imu data
+		# theres also some formatting in here
+		print("Latitude:"+str(gps.latitude)
+			, "Longitude:"+str(gps.longitude)
+			, "Altitude:" +str(gps.altitude_m)
+			, "\tSpeed:" +str(gps.speed_kmh)
+			, "Satellites:" + str(gps.satellites)
+			, "\r\n"
+			, "AccX:" +str(device.getDeviceData("accX"))
+			, "AccY:" +str(device.getDeviceData("accY"))
+			, "AccZ:" +str(device.getDeviceData("accZ"))
+			, "Roll:" +str(device.getDeviceData("angleX"))
+			, "Pitch?:" +str(device.getDeviceData("angleY"))
+			, "Yaw?:" + str(device.getDeviceData("angleZ"))
+			, "\r\n"
+    		)
+
+	# If that lovely file is open then were gonna write to it
+	if (_IsWriteF):
+
+		# Another super long string, same data as before but shoved into a big string
+		Tempstr = str("{}/{}/{} {:02}:{:02}:{:02}".format(
+				gps.timestamp_utc.tm_mon,
+				gps.timestamp_utc.tm_mday,
+				gps.timestamp_utc.tm_year,
+				gps.timestamp_utc.tm_hour,
+				gps.timestamp_utc.tm_min,
+				gps.timestamp_utc.tm_sec,
+				))
+		Tempstr += "\t" +str(gps.latitude)
+		Tempstr += "\t"+str(gps.longitude)
+		Tempstr += "\t"+str(gps.altitude_m)
+		Tempstr += "\t\t"+str(gps.speed_kmh)
+		Tempstr += "\t\t"+str(device.getDeviceData("accX"))
+		Tempstr += "\t\t"+str(device.getDeviceData("accY"))
+		Tempstr += "\t\t"+str(device.getDeviceData("accZ"))
+		Tempstr += "\t\t"+str(device.getDeviceData("angleX"))
+		Tempstr += "\t\t"+str(device.getDeviceData("angleY"))
+		Tempstr += "\t\t"+str(device.getDeviceData("angleZ"))
+		Tempstr += "\t\t" +str(gps.satellites)
+		Tempstr += "\r\n"
+		_writeF.write(Tempstr)	# Writing string to file
+
+def csvWrite(data, title, headers):
+	with open(title, mode='w', newline='') as file:
+		writer = csv.writer(file)
+		print(type(data))
+		print(type(headers))
+		writer.writerow(headers)
+		writer.writerows(data)
+	print("CSV written")
+
+
+
+# Drive safe
+# Soli deo gloria
+#    (()__(()
+#    /       \
+#   ( /    \  \
+#    \ o o    /
+#    (_()_)__/ \
+#    / _,==.____ \
+#   (   |--|      )
+#   /\_.|__|'-.__/\_
+#  / (        /     \
+#  \  \      (      /
+#   )  '._____)    /
+#(((____.--(((____/
+
