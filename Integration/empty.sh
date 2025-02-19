a57974624c5165d82c3da298974b879a969c26a6
Kaleb Binger <kalebbinger@gmail.com>
2025-02-19 00:01:50 -0700

    again

--- Integration/empty.sh
+++ Integration/empty.sh
@@ -1,5 +1,5 @@
 # this file is just to remove the extra .txt files from running the integration script
 # they build up and this cleans it quick
 
-rm 20*.txt 2> /dev/null
+rm *.txt 2> /dev/null
 rm *.csv 2> /dev/null
