56cd17e77eb80495cad3100a219e31a9144d342a
Kaleb Binger <kalebbinger@gmail.com>
2025-02-20 17:10:26 -0700

    magnetic encoder stuff

---
+++ Integration/pi1/p1Int.py
@@ -0,0 +1,40 @@
+import smbus2
+import time
+import dependencies.util_func as util_func
+
+# Sampling Frequency Stuff
+sampling_freq = 10 # Hz
+interval = 1 /sampling_freq # s
+
+# Magnetic Encoder I2C addy
+AS5600_ADDR = 0x36
+RAW_ANGLE_REG = 0x0C
+AS5600data = [[time.monotonic(), 0.0]]
+
+# Function to Read AS5600 Angle
+def read_angle():
+	bus = smbus2.SMBus(1)
+	data = bus.read_i2c_block_data(AS5600_ADDR, RAW_ANGLE_REG, 2)
+	bus.close()
+	raw_angle = (data[0] << 8 | date[1]
+	angle = (raw_angle / 4096.0) * 360.0
+	return angle
+
+# Housekeeping before recording data
+last_print = time.monotonic()
+
+# Meat of recording and printing data
+try:
+	while True:
+		current = time.monotonic()
+		if current - last_print >= interval:
+			last_print = current
+			angle = read_angle()
+			print(f"Angle: {angle:.2f} def")
+			AS5600data.append([current, angle])
+
+except KeyboardInterrupt:
+	pass
+
+# Write Data to CSV
+util_func.csvWrite(AS5600data, "AS5600", ["Time", "Angle (deg)"])
