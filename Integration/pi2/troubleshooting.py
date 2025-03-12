86aa8742a87a923787a83cccf31c386e5d36004c
Kaleb Binger <kalebbinger@gmail.com>
2025-03-11 21:36:17 -0600

    l

--- Integration/pi2/troubleshooting.py
+++ Integration/pi2/troubleshooting.py
@@ -20,44 +20,16 @@ import multiprocessing
 import lgpio
 import gc
 
-gc.disble()
+#gc.disble()
 
-# Sampling Frequency Setup
-slow_sampling_freq = 10 #Hz (GPS & IMU sampling rate)
-interval = 1/slow_sampling_freq #s GPS & IMU interval
-fast_sampling_freq = 3300 #Hz (ADS1015 sampling rate)
-interval2 = 1/fast_sampling_freq #s ADS interval
-pulse_count1 = 0
-pulse_count2 = 0
-
-# Data Storage
-GPSdata = []
-IMUdata = []
 Pot1data = []
 Pot2data = []
-Hall1data = []
-Hall2data = []
 
-def pulse_callback(chip, gpio, level, timestamp):
-	'''
-	callback function that gets called on the rising edge of the hall sensor output.
-	increments pulse counter
-	'''
-	global pulse_count1,pulse_count2, Hall1data, Hall2data
-	if level == 1:
-		if gpio == PIN1:
-			pulse_count1 +=1
-			Hall1data.append([time.perf_counter(), pulse_count1])
-#			print("Hall1:", Hall1data[-1])
-		elif gpio == PIN2:
-			pulse_count2 += 1
-			Hall2data.append([time.perf_counter(), pulse_count2])
-#			print("Hall2:", Hall2data[-1])
-
+'''
 def imu_gps_process(gps_queue, imu_queue):
-	'''Runs GPS and IMU processing in a seperate process to avoid slowing down
+	Runs GPS and IMU processing in a seperate process to avoid slowing down
 	other sampling
-	'''
+	
 	# Initialize GPS
 	GPSserial_port = '/dev/ttyUSB0'
 	GPSbaud_rate = 115200
@@ -103,9 +75,9 @@ def imu_gps_process(gps_queue, imu_queue):
 			# Put data into queues
 			gps_queue.put(GPSdata_local)
 			imu_queue.put(IMUdata_local)
-
+'''
 # Configure Hall Effect
-CHIP = 0
+'''CHIP = 0
 PIN1 = 17
 PIN2 = 27
 h = lgpio.gpiochip_open(CHIP)
@@ -116,12 +88,14 @@ lgpio.callback(h, PIN1, lgpio.RISING_EDGE, pulse_callback)
 lgpio.gpio_claim_input(h, PIN2)
 lgpio.gpio_claim_alert(h, PIN2, lgpio.RISING_EDGE)
 lgpio.callback(h, PIN2, lgpio.RISING_EDGE, pulse_callback)
-
+'''
 # Initialize ADS1015
 i2c = busio.I2C(board.SCL, board.SDA)	# Declaring I2C object
 adc = ADS.ADS1015(i2c)			# ADS1015 object
 CONFIG_REGISTER = 0x01 			# Honestly idk ask zavier
 util_func.set_continuous_mode(adc, CONFIG_REGISTER)	# Enable continuous mode
+fast_sampling_freq = 3300;
+interval2 = 1/3300;
 adc.data_rate = fast_sampling_freq	# Set ADS1015 to an appropriate sample rate (predetermined by hardware-see datasheet)
 pot_channel1 = AnalogIn(adc, ADS.P0)	# Initialize channel in Single-Ended Mode
 pot_channel2 = AnalogIn(adc, ADS.P1)	# Initialize channel 2 in Single-Ended Mode
@@ -129,13 +103,13 @@ pot_channel2 = AnalogIn(adc, ADS.P1)	# Initialize channel 2 in Single-Ended Mode
 # Housekeeping before recording data
 slast_print = time.perf_counter()	# Start time for sampling
 flast_print = slast_print
-
+'''
 # Start GPS & IMU processing
 gps_queue = multiprocessing.Queue()
 imu_queue = multiprocessing.Queue()
 gps_imu_proc = multiprocessing.Process(target=imu_gps_process, args=(gps_queue, imu_queue), daemon=True)
 gps_imu_proc.start()
-
+'''
 # Meat of recording and printing data
 try:							# Try & except to give a way of ending loop someday 
 	while True:					# While loop to continue checking sensors
@@ -149,21 +123,23 @@ try:							# Try & except to give a way of ending loop someday
 #			print(raw_value1, raw_value2)
 except KeyboardInterrupt:	# Ctrl+C sends keyboard interupt and stops loop
 	pass			# Does literally nothing but stop python from whining
-
+'''
 while not gps_queue.empty():
 	GPSdata=gps_queue.get()
 while not imu_queue.empty():
 	IMUdata=imu_queue.get()
-
+'''
 # Close serial devices
-gps_imu_proc.terminate()
+'''gps_imu_proc.terminate()
 gps_imu_proc.join(timeout=1)
 lgpio.gpiochip_close(h)
-
+'''
 # Write to file
-util_func.csvWriteUSB(GPSdata, "GPS", ["Time", "Lat", "Long", "Alt", "Speed", "Sats"])				# GPS
+'''util_func.csvWriteUSB(GPSdata, "GPS", ["Time", "Lat", "Long", "Alt", "Speed", "Sats"])				# GPS
 util_func.csvWriteUSB(IMUdata, "IMU", ["Time", "AccX", "AccY", "AccZ", "AngleX", "AngleY", "AngleZ"])		# IMU
+'''
 util_func.csvWriteUSB(Pot1data, "Pot1", ["Time", "RawValue"])
 util_func.csvWriteUSB(Pot2data, "Pot2", ["Time", "RawValue"])
-util_func.csvWriteUSB(Hall1data, "Hall1", ["Time", "PulseCounts"])
+'''util_func.csvWriteUSB(Hall1data, "Hall1", ["Time", "PulseCounts"])
 util_func.csvWriteUSB(Hall2data, "Hall2", ["Time", "PulseCounts"])
+'''
