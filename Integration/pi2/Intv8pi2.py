ae22707011ec90bf091017ddb21db806003a2240
kalebbinger <82975957+kalebbinger@users.noreply.github.com>
2025-03-28 14:08:09 -0600

    Update Intv8pi2.py

--- Integration/pi2/Intv8pi2.py
+++ Integration/pi2/Intv8pi2.py
@@ -3,6 +3,10 @@
 # F21-61 - Swap Hudson
 # v5: moved IMU and GPS to a seperate process to keep potentiometer running fast enough
 
+#############################
+# --- Required Packages --- #
+#############################
+
 import serial			# Serial to mess w sensors
 import time			# Lets us check what time it is
 import adafruit_gps		# Tells what the GPS is thinking
@@ -21,8 +25,6 @@ import lgpio
 import gc
 import can
 
-gc.disable()
-bus = can.Bus(channel='can0', bustype='socketcan', bitrate=500000)
 #########################
 # --- Configuration --- #
 #########################
@@ -41,24 +43,53 @@ IMUdata = []
 Potdata = []
 Halldata = []
 
+# Configure Hall Effect
+CHIP = 0
+PIN1 = 17
+PIN2 = 27
+h = lgpio.gpiochip_open(CHIP)
+
+lgpio.gpio_claim_input(h, PIN1)
+lgpio.gpio_claim_alert(h, PIN1, lgpio.RISING_EDGE)
+
+lgpio.gpio_claim_input(h, PIN2)
+lgpio.gpio_claim_alert(h, PIN2, lgpio.RISING_EDGE)
+
+# Initialize ADS1015
+i2c = busio.I2C(board.SCL, board.SDA)	# Declaring I2C object
+adc = ADS.ADS1015(i2c)			# ADS1015 object
+CONFIG_REGISTER = 0x01 			# Honestly idk ask zavier
+util_func.set_continuous_mode(adc, CONFIG_REGISTER)	# Enable continuous mode
+adc.data_rate = ADSsampling_freq	# Set ADS1015 to an appropriate sample rate (predetermined by hardware-see datasheet)
+pot_channel1 = AnalogIn(adc, ADS.P0)	# Initialize channel in Single-Ended Mode
+pot_channel2 = AnalogIn(adc, ADS.P1)	# Initialize channel 2 in Single-Ended Mode
+
+# Housekeeping before recording data
+last_print = time.perf_counter()	# Start time for sampling
+gc.disable()
+bus = can.Bus(channel='can0', bustype='socketcan', bitrate=500000)
+
+#####################
+# --- Functions --- #
+#####################
+
 def pulse_callback(chip, gpio, level, timestamp):
 	'''
 	callback function that gets called on the rising edge of the hall sensor output.
 	increments pulse counter
 	'''
-	global pulse_count1,pulse_count2, Hall1data, Hall2data
+	global pulse_count1,pulse_count2, Halldata
 	if level == 1:
 		if gpio == PIN1:
 			pulse_count1 +=1
-			Hall1data.append([time.perf_counter(), pulse_count1])
-#			print("Hall1:", Hall1data[-1])
+			Halldata.append([time.perf_counter(), pulse_count1, pulse_count2])
 		elif gpio == PIN2:
 			pulse_count2 += 1
-			Hall2data.append([time.perf_counter(), pulse_count2])
-#			print("Hall2:", Hall2data[-1])
+			Halldata.append([time.perf_counter(), pulse_count1, pulse_count2])
 
 def imu_gps_process(gps_queue, imu_queue):
-	'''Runs GPS and IMU processing in a seperate process to avoid slowing down
+	'''
+ 	Runs GPS and IMU processing in a separate process to avoid slowing down
 	other sampling
 	'''
 	# Initialize GPS
@@ -97,37 +128,18 @@ def imu_gps_process(gps_queue, imu_queue):
 				device.getDeviceData("angleX"),
 				device.getDeviceData("angleY"),
 				device.getDeviceData("angleZ")]
-#			print(imu_data)
-
 			# Put data into queues
 			gps_queue.put(gps_data)
 			imu_queue.put(imu_data)
 
-# Configure Hall Effect
-CHIP = 0
-PIN1 = 17
-PIN2 = 27
-h = lgpio.gpiochip_open(CHIP)
-lgpio.gpio_claim_input(h, PIN1)
-lgpio.gpio_claim_alert(h, PIN1, lgpio.RISING_EDGE)
-lgpio.callback(h, PIN1, lgpio.RISING_EDGE, pulse_callback)
-
-lgpio.gpio_claim_input(h, PIN2)
-lgpio.gpio_claim_alert(h, PIN2, lgpio.RISING_EDGE)
-lgpio.callback(h, PIN2, lgpio.RISING_EDGE, pulse_callback)
-
-# Initialize ADS1015
-i2c = busio.I2C(board.SCL, board.SDA)	# Declaring I2C object
-adc = ADS.ADS1015(i2c)			# ADS1015 object
-CONFIG_REGISTER = 0x01 			# Honestly idk ask zavier
-util_func.set_continuous_mode(adc, CONFIG_REGISTER)	# Enable continuous mode
-adc.data_rate = fast_sampling_freq	# Set ADS1015 to an appropriate sample rate (predetermined by hardware-see datasheet)
-pot_channel1 = AnalogIn(adc, ADS.P0)	# Initialize channel in Single-Ended Mode
-pot_channel2 = AnalogIn(adc, ADS.P1)	# Initialize channel 2 in Single-Ended Mode
-
-# Housekeeping before recording data
-slast_print = time.perf_counter()	# Start time for sampling
-flast_print = slast_print
+def stop_callbacks():
+    global hall1, hall2
+    lgpio.callback_cancel(hall1)
+    lgpio.callback_cancel(hall2)
+	
+###########################
+# --- Data Aquisition --- #
+###########################
 
 # Start GPS & IMU processing
 gps_queue = multiprocessing.Queue()
@@ -135,11 +147,15 @@ imu_queue = multiprocessing.Queue()
 gps_imu_proc = multiprocessing.Process(target=imu_gps_process, args=(gps_queue, imu_queue), daemon=True)
 gps_imu_proc.start()
 
+# Start hall Effects
+hall1 = lgpio.callback(h, PIN1, lgpio.RISING_EDGE, pulse_callback)
+hall2 = lgpio.callback(h, PIN2, lgpio.RISING_EDGE, pulse_callback)
+
 # Meat of recording and printing data
 try:							# Try & except to give a way of ending loop someday 
 	while True:					# While loop to continue checking sensors
 		current = time.perf_counter()		# Check current time
-		if current - flast_print >= (1/600.0):
+		if current - last_print >= (1/600.0):
 			raw_value1 = max(0, pot_channel1.value)	# Read ADC Values
 			raw_value2 = max(0, pot_channel2.value)
 			Potdata.append([current, raw_value1, raw_value2])
@@ -148,14 +164,13 @@ try:							# Try & except to give a way of ending loop someday
 			GPSdata.append(gps_queue.get())
 		while not imu_queue.empty():
 			IMUdata.append(imu_queue.get())
-#			print(raw_value1, raw_value2)
 except KeyboardInterrupt:	# Ctrl+C sends keyboard interupt and stops loop
 	pass			# Does literally nothing but stop python from whining
 
 # Close serial devices
 gps_imu_proc.terminate()
 gps_imu_proc.join(timeout=1)
-lgpio.gpiochip_close(h)
+stop_callbacks()
 
 # Write to file
 util_func.csvWriteUSB(GPSdata, "GPS", ["Time", "Lat", "Long", "Alt", "Speed", "Sats"])				# GPS
@@ -163,4 +178,8 @@ util_func.csvWriteUSB(IMUdata, "IMU", ["Time", "AccX", "AccY", "AccZ", "AngleX",
 util_func.csvWriteUSB(Potdata, "Pot", ["Time", "RawValue1", "RawValue2"])
 util_func.csvWriteUSB(Hall1data, "Hall1", ["Time", "PulseCounts"])
 util_func.csvWriteUSB(Hall2data, "Hall2", ["Time", "PulseCounts"])
+gc.collect
+
+
+lgpio.gpiochip_close(h)
 gc.collect()
